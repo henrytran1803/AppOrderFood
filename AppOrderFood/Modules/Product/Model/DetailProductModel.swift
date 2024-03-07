feaf3a74bf92e8345bfb9b3e9237415f657f12c3
Trần Việt Anh <tranvietanh1803@gmail.com>
2024-03-07 15:24:40 +0700

    detail product

--- AppOrderFood/Modules/Product/Model/DetailProductModel.swift
+++ AppOrderFood/Modules/Product/Model/DetailProductModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+struct DetailProductModel {
+    let id: UUID
+    let name: String
+    let category : String
+    let image : String
+    let price: Double
+    let quality: Int
+    init(id: UUID, name: String, category: String, image: String, price: Double, quality: Int) {
+        self.id = id
+        self.name = name
+        self.category = category
+        self.image = image
+        self.price = price
+        self.quality = quality
+    }
+}
+
+
+struct MockDetailProduct {
+    static let product = DetailProductModel(id: UUID(), name: "Hamburger sieu to", category: "hamburger", image: "hamburger", price: 100, quality: 20)
+    
+    
+    
+}
--- AppOrderFood/Modules/Product/View/DetailProductView.swift
+++ AppOrderFood/Modules/Product/View/DetailProductView.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct DetailProductView: View {
+    @State var product = MockDetailProduct.product
+    @State var isAddToCart = false
+    @State var quality = 1
+    @State var isMuch = false
+    @State private var selection = "M"
+    @State var selectionValue = 0
+        let size: [String: Double] = ["M": 1, "S": 2, "L": 3, "XL": 4]
     var body: some View {
         VStack {
-            Image("hamburger")
+            Image(product.image)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
             .frame(height: 400, alignment: .topLeading)
@@ -18,42 +25,104 @@ struct DetailProductView: View {
         Divider()
             .frame(height: 3)
             .overlay(.black)
-        Text("Name burger")
-        Text("Categry")
-            Text("Price: 1232123")
+            HStack {
+                Text(product.name)
+                    .font(.title)
+                    .padding()
+            }.frame(maxWidth: .infinity,alignment: .leading)
+            HStack{
+                Text(product.category)
+                    .font(.subheadline)
+                Spacer()
+                let fortmatPrice = String(format: "%.1f", product.price * (size[selection] ?? product.price))
+                Text("Price: \(fortmatPrice) VND")
+                    .foregroundColor(.red)
+            }.padding()
+            
             Divider()
             HStack{
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Text("Size")
-                })
                 HStack{
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Text("+")
+                    Text("Size")
+                        .padding(.leading)
+                        
+                    Divider().frame(height: 40)
+                    Picker("Select a size", selection: $selection) {
+                        ForEach(Array(size.keys.sorted()), id: \.self) { key in
+                            Text("\(key)")
+                        }
+                    }
+                    .pickerStyle(.menu)
+                    .foregroundColor(.blue)
+                }.background(Color.blue.secondary)
+                .cornerRadius(10)
+                .padding()
+                Spacer()
+                HStack{
+                    Button(action:  {
+                        if(quality <= 1){
+                            quality = 1
+                        }else{
+                            quality -= 1
+                        }
+                    }, label: {
+                        Text("-")
                     })
                     .padding(.leading)
                     .foregroundColor(.black)
-                    Divider().frame(height: 20)
-                    Text("1")
-                    Divider().frame(height: 20)
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Text("-")
+                    Divider().frame(height: 40)
+                    
+                    Text("\(quality)")
+                        .font(.system(size: 20))
+                    Divider().frame(height: 40)
+                    Button(action: {
+                        if(product.quality <= quality){
+                            quality = product.quality
+                            isMuch = true
+                            
+                        }else{
+                            quality += 1
+                            isMuch = false
+                        }
+                        
+                        
+                    }, label: {
+                        Text("+")
                     })
                     .padding(.trailing)
                     .foregroundColor(.black)
-                    //.frame(width: 20.0, height: 20.0)
                     
                 }.background(Color.yellow).cornerRadius(10)
+                    .padding()
+                
+            }
+            HStack{
+                Spacer()
+                Text(isMuch ? "Out of Stock":"" )
+                    .font(.system(size: 10))
+                    .foregroundColor(.red)
+                    .padding(.trailing)
             }
             Spacer()
             Divider()
             HStack{
-                Text("Total : 23213")
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Text("ADD TO CART")
-                })
+                let fortmatTotal = String(format: "%.1f", product.price * Double(quality) * (size[selection] ?? product.price * Double(quality)))
+                Text("Total : \(fortmatTotal) VND")
+                    .font(.system(size: 20).bold())
+                    .padding(.leading)
+                Spacer()
+                Button(action: {isAddToCart = true}, label: {
+                    Text(isAddToCart ? "ADD COMPLETE":"ADD TO CART")
+                    Image(systemName: isAddToCart ? "checkmark.seal.fill":"cart")
+                    
+                }).padding()
+                .background(isAddToCart ? Color.green: Color.pink)
+                .cornerRadius(20)
+                .foregroundColor(.white)
+                .bold()
+                .frame(maxWidth: .infinity)
+                .animation(.easeInOut(duration: 1),value: isAddToCart)
+        
             }
-            Divider()
-            
         }
 
     }
