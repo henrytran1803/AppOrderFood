9c9a805ebe2d52cffa4df8c58013712cc760d460
Trần Việt Anh <tranvietanh1803@gmail.com>
2024-04-09 21:26:53 +0700

    cycle

--- AppOrderFood/Modules/Payment/View/RuleView.swift
+++ AppOrderFood/Modules/Payment/View/RuleView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct RuleView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text("Hello, World!")
+                    .onAppear {
+                        print("🔵 onAppear")
+                    }
+                    .onDisappear {
+                        print("🔵 onDisappear")
+                    }
     }
 }
 
